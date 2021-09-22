b1cd02e54c9cd36898d017cba9514206177205a9
Danyyl Nartov <danyylnartov21@gmail.com>
2021-09-22 12:41:04 +0300

    4 lesson, class Registry

---
+++ config/params.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+	'admin_email' => 'danyylnartov21@gmail.com',
+	'shop_name' => 'Магазин webShop',
+	'pagination' => 3,
+	'smtp_login' => '',
+	'smtp_password' => '',
+];
--- public/index.php
+++ public/index.php
@@ -1,3 +1,7 @@
 <?php
 
 require_once dirname(__DIR__) . '/config/init.php';
+require_once LIBS . '/functions.php';
+
+$app = new \webShop\App();
+debug(\webShop\App::$app->getProperties());
