38e3b63ace3798b833184cbf7bcf0fc46d5441a7
Danyyl Nartov <danyylnartov21@gmail.com>
2021-09-23 13:06:50 +0300

    13 lesson, class Cache

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -5,17 +5,24 @@ namespace app\controllers;
 
 
 use RedBeanPHP\R;
+use webShop\Cache;
 
 class MainController extends AppController {
 
 	public function indexAction() {
 		$posts = R::findAll('test');
 		$post = R::findOne('test', 'id = ?', [2]);
-		debug($post);
 		$this->setMeta('Главная страница', 'Описание...', 'Ключевики');
 		$name = 'John';
 		$age = 30;
-		$names = ['Andre', 'Jane'];
+		$names = ['Andre', 'Jane', 'Mike'];
+		$cache = Cache::instance();
+//		$cache->delete('test');
+		$data = $cache->get('test');
+		if (!$data) {
+			$cache->set('test', $names);
+		}
+		debug($data);
 		$this->set(compact('name', 'age', 'names', 'posts'));
 	}
 
