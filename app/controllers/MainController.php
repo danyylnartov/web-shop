5007168ab6f5d492e5d7547dbfd90524d9dd297f
Danyyl Nartov <danyylnartov21@gmail.com>
2021-09-23 17:57:36 +0300

    16 lesson, showing brands

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -10,7 +10,9 @@ use webShop\Cache;
 class MainController extends AppController {
 
 	public function indexAction() {
+		$brands = R::find('brand', 'LIMIT 3');
 		$this->setMeta('Главная страница', 'Описание...', 'Ключевики');
+		$this->set(compact('brands'));
 	}
 
 }
