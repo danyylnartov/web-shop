181880f96fd3e91a3e1b2186f56ac628eb8f86ef
Danyyl Nartov <danyylnartov21@gmail.com>
2021-09-23 09:00:16 +0300

    10 lesson, class View second part

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -7,8 +7,12 @@ namespace app\controllers;
 class MainController extends AppController {
 
 	public function indexAction() {
-		echo __METHOD__;
-
+//		echo __METHOD__;
+		$this->setMeta('Главная страница', 'Описание...', 'Ключевики');
+		$name = 'John';
+		$age = 30;
+		$names = ['Andre', 'Jane'];
+		$this->set(compact('name', 'age', 'names'));
 	}
 
 }
