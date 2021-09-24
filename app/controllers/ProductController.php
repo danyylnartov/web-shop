95ce09e8473e226b6adb885d5e606ea2e17fba77
Danyyl Nartov <danyylnartov21@gmail.com>
2021-09-24 19:51:19 +0300

    28 lesson, Card Product, Module viewed products

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -4,6 +4,7 @@
 namespace app\controllers;
 
 
+use app\models\Product;
 use RedBeanPHP\R;
 
 class ProductController extends AppController {
@@ -23,18 +24,26 @@ class ProductController extends AppController {
 
 
 		// Запись в куки запрошенного товара
+		$p_model = new Product();
+		$p_model->setRecentlyViewed($product->id);
+
 
 		// просмотренные товары
+
+		$r_viewed = $p_model->getRecentlyViewed();
+		$recentlyViewed = null;
+		if ($r_viewed) {
+			$recentlyViewed = R::find('product', 'id IN (' . R::genSlots($r_viewed) . ') LIMIT 3', $r_viewed);
+		}
 
 		// галерея
 
 		$gallery = R::findAll('gallery', 'product_id = ?', [$product->id]);
-		debug($gallery);
 
 		// модификации
 
 		$this->setMeta($product->title, $product->description, $product->keywords);
-		$this->set(compact(['product', 'related', 'gallery']));
+		$this->set(compact(['product', 'related', 'gallery', 'recentlyViewed']));
 
 	}
 
