82fcfea19437597e5f6def9fd317ecfb89d1b515
Danyyl Nartov <danyylnartov21@gmail.com>
2021-09-24 15:41:49 +0300

    24 lesson, Card Product, first part

---
+++ app/controllers/ProductController.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace app\controllers;
+
+
+use RedBeanPHP\R;
+
+class ProductController extends AppController {
+
+	public function viewAction() {
+		$alias = $this->route['alias'];
+		$product = R::findOne('product', "alias = ? AND status = '1'", [$alias]);
+		if (!$product) {
+			throw new \Exception('Страница не найдена', 404);
+		}
+
+		// Bread crumbs
+
+		// Связанные товары
+
+		// Запись в куки запрошенного товара
+
+		// просмотренные товары
+
+		// галерея
+
+		// модификации
+
+		$this->setMeta($product->title, $product->description, $product->keywords);
+		$this->set(compact(['product']));
+
+	}
+
+}
