a3cff0b2a9af7d91a8bf4cb849de154ec40b4c48
Danyyl Nartov <danyylnartov21@gmail.com>
2021-09-25 18:14:49 +0300

    34 lesson, Cart, fourth part

--- public/js/main.js
+++ public/js/main.js
@@ -18,6 +18,21 @@ $('body').on('click', '.add-to-cart-link', function (e) {
     });
 });
 
+$('#cart .modal-body').on('click', '.del-item', function () {
+    var id = $(this).data('id');
+    $.ajax({
+       url: '/cart/delete',
+       data: {id: id},
+       type: 'GET',
+       success: function (res) {
+           showCart(res);
+       },
+        error: function () {
+           alert('Eroor!');
+        }
+    });
+});
+
 function showCart(cart) {
     if ($.trim(cart) == '<h3>Корзина пуста</h3>') {
         $('#cart .modal-footer a, #cart .modal-footer .btn-danger').css('display', 'none');
@@ -26,6 +41,24 @@ function showCart(cart) {
     }
     $('#cart .modal-body').html(cart);
     $('#cart').modal();
+    if ($('.cart-sum').text()) {
+        $('.simpleCart_total').html($('#cart .cart-sum').text());
+    } else {
+        $('.simpleCart_total').text('Empty Cart');
+    }
+}
+
+function getCart() {
+    $.ajax({
+        url: '/cart/show',
+        type: 'GET',
+        success: function (res) {
+            showCart(res);
+        },
+        error: function () {
+            alert('Ошибка! Попробуйте позже');
+        }
+    });
 }
 
 /* Cart */
