cce89c14b2f3472152def5eb6e8972d72972c4bc
Danyyl Nartov <danyylnartov21@gmail.com>
2021-09-28 08:45:44 +0300

    48 lesson, Ordering product, fourth part

--- config/params.php
+++ config/params.php
@@ -1,9 +1,12 @@
 <?php
 
 return [
-	'admin_email' => 'danyylnartov21@gmail.com',
+	'admin_email' => 'g.am.err377.7@gmail.com',
 	'shop_name' => 'Магазин webShop',
 	'pagination' => 3,
-	'smtp_login' => '',
-	'smtp_password' => '',
+	'smtp_host' => 'smtp.gmail.com',
+	'smtp_port' => '465',
+	'smtp_protocol' => 'SSL',
+	'smtp_login' => 'danyylnartov21@gmail.com',
+	'smtp_password' => '11ZB*PSza&Q',
 ];
