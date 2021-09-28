04edc8f01ec647c953583445f50b3515a0730bf2
Danyyl Nartov <danyylnartov21@gmail.com>
2021-09-28 16:04:37 +0300

    52 lesson, Filters, fourth part, first version filters

--- app/widgets/filter/Filter.php
+++ app/widgets/filter/Filter.php
@@ -36,6 +36,10 @@ class Filter {
 
 	protected function getHtml() {
 		ob_start();
+		$filter = self::getFilter();
+		if (!empty($filter)) {
+			$filter = explode(',', $filter);
+		}
 		require $this->tpl;
 		return ob_get_clean();
 	}
