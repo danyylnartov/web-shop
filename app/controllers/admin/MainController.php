fa9fe5770b1f6d2b3bfa71b598096b67081456a2
Danyyl Nartov <danyylnartov21@gmail.com>
2021-10-03 13:30:01 +0300

    Third module, 56 lesson, Home page widgets

--- app/controllers/admin/MainController.php
+++ app/controllers/admin/MainController.php
@@ -4,10 +4,17 @@
 namespace app\controllers\admin;
 
 
+use RedBeanPHP\R;
+
 class MainController extends AppController {
 
 	public function indexAction() {
+		$countNewOrders = R::count('order', "status = '0'");
+		$countUsers = R::count('user');
+		$countProducts = R::count('product');
+		$countCategories = R::count('category');
 		$this->setMeta('Панель управления');
+		$this->set(compact('countNewOrders', 'countCategories', 'countProducts', 'countUsers'));
 	}
 
 }
