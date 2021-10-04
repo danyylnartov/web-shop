e44878ab8fceac64532c0cc410d1141b4d4c0b7b
Danyyl Nartov <danyylnartov21@gmail.com>
2021-10-04 11:03:42 +0300

    Third module, 61 lesson, Category management, second part

--- public/adminlte/my.js
+++ public/adminlte/my.js
@@ -2,3 +2,11 @@ $('.delete').click(function () {
    var res = confirm('Потвердите действие');
    if (!res) return false;
 });
+
+$('.nav-sidebar a').each(function () {
+   var location = window.location.protocol + '//' + window.location.host + window.location.pathname;
+   var link = this.href;
+   if (link == location) {
+      $(this).addClass('active');
+   }
+});
