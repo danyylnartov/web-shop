ca1831a58d350c9f9b18b5748ec5e3c067381291
Danyyl Nartov <danyylnartov21@gmail.com>
2021-10-07 15:25:53 +0300

    Third module, 83 lesson, Filter management, third part

--- app/controllers/admin/FilterController.php
+++ app/controllers/admin/FilterController.php
@@ -10,8 +10,7 @@ use RedBeanPHP\R;
 
 class FilterController extends AppController {
 
-	public function groupDeleteAction()
-	{
+	public function groupDeleteAction() {
 		$id = $this->getRequestID();
 		$count = R::count('attribute_value', 'attr_group_id = ?', [$id]);
 		if ($count) {
@@ -21,6 +20,35 @@ class FilterController extends AppController {
 		R::exec('DELETE FROM attribute_group WHERE id = ?', [$id]);
 		$_SESSION['success'] = 'Удалено';
 		redirect();
+	}
+
+	public function attributeDeleteAction() {
+		$id = $this->getRequestID();
+		R::exec("DELETE FROM attribute_product WHERE  attr_id = ?", [$id]);
+		R::exec("DELETE FROM attribute_value WHERE id = ?", [$id]);
+		$_SESSION['success'] = 'Удалено';
+		redirect();
+	}
+
+	public function groupEditAction() {
+		if (!empty($_POST)) {
+			$id = $this->getRequestID(false);
+			$group = new FilterGroup();
+			$data = $_POST;
+			$group->load($data);
+			if (!$group->validate($data)) {
+				$group->getErrors();
+				redirect();
+			}
+			if ($group->update('attribute_group', $id)) {
+				$_SESSION['success'] = 'Изменения сохранены';
+				redirect();
+			}
+		}
+		$id = $this->getRequestID();
+		$group = R::load('attribute_group', $id);
+		$this->setMeta("Редактирование группы {$group->title}");
+		$this->set(compact('group'));
 	}
 
 	public function groupAddAction() {
@@ -44,6 +72,28 @@ class FilterController extends AppController {
 		$attrs_group = R::findAll('attribute_group');
 		$this->setMeta('Группы фильтров');
 		$this->set(compact('attrs_group'));
+	}
+
+	public function attributeEditAction() {
+		if (!empty($_POST)) {
+			$id	= $this->getRequestID(false);
+			$attr = new FilterAttr();
+			$data = $_POST;
+			$attr->load($data);
+			if (!$attr->validate($data)) {
+				$attr->getErrors();
+				redirect();
+			}
+			if ($attr->update('attribute_value', $id)) {
+				$_SESSION['success'] = 'Изменения сохранены';
+				redirect();
+			}
+		}
+		$id	= $this->getRequestID();
+		$attr = R::load('attribute_value', $id);
+		$attrs_group = R::findAll('attribute_group');
+		$this->setMeta('Редактирование атрибута');
+		$this->set(compact('attr', 'attrs_group'));
 	}
 
 	public function attributeAddAction() {
